cb97b04f0c8fbcd0c977ac5fc4968c9911c27396
Karn Wong <karn@karnwong.me>
2024-08-09 15:18:14 +0700

    add stub for extractData

--- cmd/wakatime.go
+++ cmd/wakatime.go
@@ -1,15 +1,31 @@
 package cmd
 
 import (
+	"context"
 	"encoding/base64"
 	"fmt"
+	"net/http"
 	"time"
 
+	"github.com/carlmjohnson/requests"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
 
 var WakatimeEndpoint = "https://wakatime.com"
 
+type WakatimeKeyStats []struct {
+	Name         string      `json:"name"`
+	TotalSeconds float64     `json:"total_seconds"`
+	Color        interface{} `json:"color"`
+	Digital      string      `json:"digital"`
+	Decimal      string      `json:"decimal"`
+	Text         string      `json:"text"`
+	Hours        int         `json:"hours"`
+	Minutes      int         `json:"minutes"`
+	Seconds      int         `json:"seconds"`
+	Percent      float64     `json:"percent"`
+}
 type WakatimeStats struct {
 	Data []struct {
 		GrandTotal struct {
@@ -27,85 +43,13 @@ type WakatimeStats struct {
 			Text     string    `json:"text"`
 			Timezone string    `json:"timezone"`
 		} `json:"range"`
-		Projects []struct {
-			Name         string      `json:"name"`
-			TotalSeconds float64     `json:"total_seconds"`
-			Color        interface{} `json:"color"`
-			Digital      string      `json:"digital"`
-			Decimal      string      `json:"decimal"`
-			Text         string      `json:"text"`
-			Hours        int         `json:"hours"`
-			Minutes      int         `json:"minutes"`
-			Seconds      int         `json:"seconds"`
-			Percent      float64     `json:"percent"`
-		} `json:"projects"`
-		Languages []struct {
-			Name         string  `json:"name"`
-			TotalSeconds float64 `json:"total_seconds"`
-			Digital      string  `json:"digital"`
-			Decimal      string  `json:"decimal"`
-			Text         string  `json:"text"`
-			Hours        int     `json:"hours"`
-			Minutes      int     `json:"minutes"`
-			Seconds      int     `json:"seconds"`
-			Percent      float64 `json:"percent"`
-		} `json:"languages"`
-		Dependencies []struct {
-			Name         string  `json:"name"`
-			TotalSeconds float64 `json:"total_seconds"`
-			Digital      string  `json:"digital"`
-			Decimal      string  `json:"decimal"`
-			Text         string  `json:"text"`
-			Hours        int     `json:"hours"`
-			Minutes      int     `json:"minutes"`
-			Seconds      int     `json:"seconds"`
-			Percent      float64 `json:"percent"`
-		} `json:"dependencies"`
-		Machines []struct {
-			Name          string  `json:"name"`
-			TotalSeconds  float64 `json:"total_seconds"`
-			MachineNameID string  `json:"machine_name_id"`
-			Digital       string  `json:"digital"`
-			Decimal       string  `json:"decimal"`
-			Text          string  `json:"text"`
-			Hours         int     `json:"hours"`
-			Minutes       int     `json:"minutes"`
-			Seconds       int     `json:"seconds"`
-			Percent       float64 `json:"percent"`
-		} `json:"machines"`
-		Editors []struct {
-			Name         string  `json:"name"`
-			TotalSeconds float64 `json:"total_seconds"`
-			Digital      string  `json:"digital"`
-			Decimal      string  `json:"decimal"`
-			Text         string  `json:"text"`
-			Hours        int     `json:"hours"`
-			Minutes      int     `json:"minutes"`
-			Seconds      int     `json:"seconds"`
-			Percent      float64 `json:"percent"`
-		} `json:"editors"`
-		OperatingSystems []struct {
-			Name         string  `json:"name"`
-			TotalSeconds float64 `json:"total_seconds"`
-			Digital      string  `json:"digital"`
-			Decimal      string  `json:"decimal"`
-			Text         string  `json:"text"`
-			Hours        int     `json:"hours"`
-			Minutes      int     `json:"minutes"`
-			Seconds      int     `json:"seconds"`
-			Percent      float64 `json:"percent"`
-		} `json:"operating_systems"`
-		Categories []struct {
-			Name         string  `json:"name"`
-			TotalSeconds float64 `json:"total_seconds"`
-			Digital      string  `json:"digital"`
-			Decimal      string  `json:"decimal"`
-			Text         string  `json:"text"`
-			Hours        int     `json:"hours"`
-			Minutes      int     `json:"minutes"`
-			Seconds      int     `json:"seconds"`
-			Percent      float64 `json:"percent"`
-		} `json:"categories"`
+		Projects         WakatimeKeyStats `json:"projects"`
+		Languages        WakatimeKeyStats `json:"languages"`
+		Dependencies     WakatimeKeyStats `json:"dependencies"`
+		Machines         WakatimeKeyStats `json:"machines"`
+		Editors          WakatimeKeyStats `json:"editors"`
+		OperatingSystems WakatimeKeyStats `json:"operating_systems"`
+		Categories       WakatimeKeyStats `json:"categories"`
 	} `json:"data"`
 	Start           time.Time `json:"start"`
 	End             time.Time `json:"end"`
@@ -126,11 +70,60 @@ type WakatimeStats struct {
 	} `json:"daily_average"`
 }
 
-type Stats struct {
+type CategoryStats struct {
 	Slug    string
 	Percent float64
 }
 
+type Stats struct {
+	Projects  []CategoryStats
+	Languages []CategoryStats
+	Editors   []CategoryStats
+	OS        []CategoryStats
+}
+
 func createAuthorizationHeader() string {
 	return fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte(viper.GetString("WAKATIME_API_KEY"))))
+}
+
+func getStats(period string) WakatimeStats {
+	var response WakatimeStats
+	err := requests.
+		URL(WakatimeEndpoint).
+		Method(http.MethodGet).
+		Path("api/v1/users/current/summaries").
+		Param("range", period).
+		Header("Authorization", createAuthorizationHeader()).
+		ToJSON(&response).
+		Fetch(context.Background())
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to get stats for today")
+	}
+
+	return response
+}
+
+func appendToKey(keyStats WakatimeKeyStats) []CategoryStats {
+	var categoryStats []CategoryStats
+
+	for _, i := range keyStats {
+		categoryStats = append(categoryStats, CategoryStats{
+			Slug:    fmt.Sprintf("%s (%s)", i.Name, i.Text),
+			Percent: i.Percent,
+		})
+	}
+
+	return categoryStats
+}
+func extractData(r WakatimeStats) Stats {
+	var stats Stats
+
+	for _, i := range r.Data {
+		stats.Projects = appendToKey(i.Projects)
+		stats.Languages = appendToKey(i.Languages)
+		stats.Editors = appendToKey(i.Editors)
+		stats.OS = appendToKey(i.OperatingSystems)
+	}
+
+	return stats
 }
