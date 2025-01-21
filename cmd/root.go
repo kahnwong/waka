b79ff42fa258be0ad761a7b033f1508357fa1060
Karn Wong <karn@karnwong.me>
2025-01-21 13:28:56 +0700

    chore: refactor to use cli-base

--- cmd/root.go
+++ cmd/root.go
@@ -7,9 +7,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"path/filepath"
 	"strings"
 
+	cliBase "github.com/kahnwong/cli-base"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -28,8 +28,6 @@ var rootCmd = &cobra.Command{
 	Use:     "waka",
 	Version: version,
 	Short:   "Display wakatime stats in your terminal",
-	//Run: func(cmd *cobra.Command, args []string) {
-	//},
 }
 
 func Execute() {
@@ -39,56 +37,43 @@ func Execute() {
 	}
 }
 
-func init() {
-	// read config
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath("$HOME/.config/waka")
+func InitConfigIfNotExists(path string) {
+	// get API key from user input
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter Wakatime API Key: ")
 
-	err := viper.ReadInConfig()
+	apiKey, err := reader.ReadString('\n')
 	if err != nil {
-		// get API key from user input
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Enter Wakatime API Key: ")
-
-		apiKey, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatal().Err(err).Msg("Error reading user input")
-		}
-
-		// write config
-		//// create config dir
-		homeDir, err := os.UserHomeDir()
-		if err != nil {
-			log.Fatal().Err(err).Msg("Error obtaining home directory")
-		}
-		err = os.MkdirAll(filepath.Join(homeDir, ".config", "waka"), os.ModePerm)
-		if err != nil {
-			log.Fatal().Err(err).Msg("Error creating config path")
-		}
-
-		//// write yaml
-		filename := filepath.Join(homeDir, ".config", "waka", "config.yaml")
-		file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-		if err != nil {
-			log.Fatal().Err(err).Msg("Error creating config file")
-		}
-		defer file.Close()
-
-		enc := yaml.NewEncoder(file)
-		err = enc.Encode(Config{WakatimeApiKey: strings.TrimSpace(apiKey)})
-
-		if err != nil {
-			log.Fatal().Err(err).Msg("Error writing config")
-		}
-
-		// read config to viper
-		err = viper.ReadInConfig()
-		if err != nil {
-			log.Fatal().Err(err).Msg("Error reading config after config initialization")
-		}
-
+		log.Fatal().Msg("Error reading user input")
 	}
+
+	// write to yaml
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatal().Msg("Error creating config file")
+	}
+	defer file.Close()
+
+	enc := yaml.NewEncoder(file)
+	err = enc.Encode(Config{WakatimeApiKey: strings.TrimSpace(apiKey)})
+
+	if err != nil {
+		log.Fatal().Msg("Error writing config")
+	}
+}
+
+func init() {
+	// init config if does not exists
+	path := "~/.config/waka/config.yaml"
+	path, err := cliBase.CheckIfConfigExists(path)
+	if err != nil {
+		InitConfigIfNotExists(path)
+		log.Info().Msg("Successfully initialized config")
+	}
+
+	// read config
+	config := cliBase.ReadYaml[Config](path)
+	viper.SetDefault("WAKATIME_API_KEY", config.WakatimeApiKey)
 
 	// rootCmd
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
